414aca23561ced86bfc7a51e033719a57758c42d
Andre Silva <andreswebs@pm.me>
2022-01-11 13:11:22 -0300

    update module name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   ecs_instance_name = "ecs-sftp-instance"
 }
 
-module "iam_ecs_instance" {
+module "iam" {
   source       = "andreswebs/ec2-role/aws"
   version      = "1.0.0"
   role_name    = local.ecs_instance_name
@@ -33,6 +33,6 @@ module "sftp" {
   sftp_users                  = var.sftp_users
   sftp_main_container_image   = var.sftp_main_container_image
   sftp_config_container_image = var.sftp_config_container_image
-  instance_profile            = module.iam_ecs_instance.instance_profile.name
+  instance_profile            = module.iam.instance_profile.name
   ssh_key_name                = module.ssh.key_pair.key_name
 }
